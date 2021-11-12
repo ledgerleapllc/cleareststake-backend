040531f1d2d6e09dd8dc14c4068398cccc704a96
= <=>
2021-11-12 12:33:35 -0500

    skjdhk

--- tests/Unit/CriticalFunctionsTest.php
+++ tests/Unit/CriticalFunctionsTest.php
@@ -35,7 +35,7 @@ final class CriticalFunctionsTest extends TestCase
 
     public function testresetUserPassword(): void
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__.'/../../bootstrap/app.php';
         $hash = Hash::make('password');
         echo $hash;
     }
