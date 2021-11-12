8d9643c728013dae36cd376fc5871ed8d711a349
= <=>
2021-11-12 13:04:53 -0500

    asdhkjha

---
+++ tests/Unit/CriticalFunctionsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class CriticalFunctionsTest extends TestCase
+{
+    public function testresetUserPassword(): void
+    {
+        $this->assertEquals(1,1);
+    }
+}
